a6663a1ef92ef7357e2ffa55368f4ebf32005197
ankur24s <youremail@example.com>
2025-01-24 09:58:49 +0530

    udpate:

--- Day15/regexp.sql
+++ Day15/regexp.sql
@@ -2,3 +2,11 @@
     regexp_instr(price,'^\\$[0-9][0-9\\.]+$'),
     price from raw.airbnb.raw_listings
 
+---------------
+
+  - name: price
+            tests:
+              - dbt_expectations.expect_column_values_to_match_regex:
+                  regex: "^\\\\$[0-9][0-9\\\\.]+$"
+				  
+----------------------
